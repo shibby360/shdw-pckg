c966b0b26257a77c183fcc87fbca6ecb63b63922
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-05-09 21:59:48 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as fhandle:
+    long_description = fhandle.read() # Your README.md file will be used as the long description!
 
 setuptools.setup(
-    name="Shdw",
-    version="0.0.1",
-    author="ShivC",
-    author_email="system.io64@gmail.com",
-    description="A package for random functions",
+    name="shdw-pckg", # Put your username here!
+    version="0.0.15", # The version of your package!
+    author="shibby360", # Your name here!
+    author_email="shivank.chhaya@gmail.com", # Your e-mail here!
+    description="a package for random functions", # A short description here!
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/shibby360/shdwdb", # Link your package website here! (most commonly a GitHub repo)
+    packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
+    ], # Enter meta data into the classifiers list!
+    python_requires='>=3.8.18', # The version requirement for Python to run your package!
 )
